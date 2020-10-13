9dd7299e4b92dad4879849316dea295ac227d0c0
andriianak <kandriiana@gmail.com>
2020-10-13 14:43:45 -0700

    add default constructor, getters, setters

--- src/edu/gonzaga/Main.java
+++ src/edu/gonzaga/Main.java
@@ -4,18 +4,53 @@ class Adder {
     int val1;
     int val2;
 
+    //defautl constructor
+    public Adder()
+    {
+        val1 = 0;
+        val2 = 0;
+    }
+
     public Adder(int new_val1, int new_val2) {
         val1 = new_val1;
         val2 = new_val2;
     }
 
+    //setter
+    public void setFirstVal(int newVal)
+    {
+        val1 = newVal;
+    }
+
+    //getter
+    public int getFirstVal()
+    {
+        return val1;
+    }
+
+    //setter
+    public vod setSecondVal(int newVal)
+    {
+        val2 = newVal;
+    }
+
+    //getter
+    public int getSecondVal()
+    {
+        return val2;
+    }
+
+
 
 }
+
+
 
 public class Main {
     public static void main(String args[]) {
         System.out.println("Starting app");
         Adder adder = new Adder(10, 20);
+        
 
         System.out.println("App finished.");
     }
