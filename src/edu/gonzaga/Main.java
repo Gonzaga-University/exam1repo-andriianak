eaf8dc7e521b6e52b3ee33f7f879b7d8ae875248
andriianak <kandriiana@gmail.com>
2020-10-13 14:47:44 -0700

    add print function

--- src/edu/gonzaga/Main.java
+++ src/edu/gonzaga/Main.java
@@ -45,8 +45,12 @@ class Adder {
     {
         return val1 + val2;
     }
-
-
+    //prints sum
+    public void printSum()
+    {
+        int sum = add();
+        return System.out.println(val1 + " + " + val2 + " = " + sum);
+    }
 }
 
 
@@ -55,7 +59,13 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("Starting app");
         Adder adder = new Adder(10, 20);
-
+        Adder adder1 = new Adder(325, 2346);
+        Adder adder2 = new Adder(90, 72);
+        Adder adder3 = new Adder();
+        adder.printSum();
+        adder1.printSum();
+        adder2.printSum();
+        adder3.printSum();
 
         System.out.println("App finished.");
     }
